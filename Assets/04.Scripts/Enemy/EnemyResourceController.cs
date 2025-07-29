98c0312ac52ec7169e145906b334039f38408021
gwangho69 <ckrhkdgh22@gmail.com>
2025-07-29 20:25:18 +0900

    [Script] 적 스크립트 작성

--- Assets/04.Scripts/Enemy/EnemyResourceController.cs
+++ Assets/04.Scripts/Enemy/EnemyResourceController.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class EnemyResourceController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float healthChangeDelay = .5f;
+
+    private EnemyBaseController baseController;
+    private EnemyStatHandler statHandler;
+    private EnemyAnimationHandler animationHandler;
+
+    private float timeSinceLastChange = float.MaxValue;
+
+    public float CurrentHealth { get; private set; }
+    public float MaxHealth => statHandler.Health;
+
+    private void Awake()
     {
-        
+        statHandler = GetComponent<EnemyStatHandler>();
+        animationHandler = GetComponent<EnemyAnimationHandler>();
+        baseController = GetComponent<EnemyBaseController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        CurrentHealth = statHandler.Health;
+    }
+
+    private void Update()
+    {
+        if (timeSinceLastChange < healthChangeDelay)
+        {
+            timeSinceLastChange += Time.deltaTime;
+            if (timeSinceLastChange >= healthChangeDelay)
+            {
+                animationHandler.InvincibilityEnd();
+            }
+        }
+    }
+
+    public bool ChangeHealth(float change)
+    {
+        if (change == 0 || timeSinceLastChange < healthChangeDelay)
+        {
+            return false;
+        }
+
+        timeSinceLastChange = 0f;
+        CurrentHealth += change;
+        CurrentHealth = CurrentHealth > MaxHealth ? MaxHealth : CurrentHealth;
+        CurrentHealth = CurrentHealth < 0 ? 0 : CurrentHealth;
+
+        if (change < 0)
+        {
+            animationHandler.Damage();
+
+        }
+
+        if (CurrentHealth <= 0f)
+        {
+            Death();
+        }
+
+        return true;
+    }
+
+    private void Death()
+    {
+        baseController.Death();
     }
 }
