8ea9c48130559b80cde0cf6e5b8b95a9cba61cdb
wooyoung-1 <wooyoung7749@gmail.com>
2025-07-30 12:49:48 +0900

    [Fix] 타일간격수정_오브젝트추가

--- Assets/04.Scripts/Field/TileSpawner.cs
+++ Assets/04.Scripts/Field/TileSpawner.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using static UnityEditor.PlayerSettings;
+using System.Security.Cryptography;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class TileSpawner : MonoBehaviour
 {
     public GameObject tilePrefab;
+    public Sprite[] tileSprites;
 
     private int height = 10;
     private int width = 10;
@@ -23,28 +26,55 @@ public class TileSpawner : MonoBehaviour
         Vector3 startPos = new Vector3(transform.position.x, transform.position.y + 4.69f, transform.position.z);
         int i = width * height;
 
-        for (int y = width - 1; y >= 0; y--)
+        for (int y = height - 1; y >= 0; y--)
         {
-            for (int x = height - 1; x >= 0; x--)
+            for (int x = width - 1; x >= 0; x--)
             {
-                float xPos = (x - y) * tileWidth / 2f;
-                float yPos = (x + y) * tileHeight / 2f;
+                float xPos = (x - y) * tileWidth / 2.06f;
+                float yPos = (x + y) * tileHeight / 2.06f;
 
                 Vector3 spawnPos = startPos + new Vector3(xPos, yPos, 0f);
 
                 GameObject tile = Instantiate(tilePrefab, spawnPos, Quaternion.identity, transform);
 
                 SpriteRenderer sr = tile.GetComponentInChildren<SpriteRenderer>();
-                if (sr != null)
+                sr.sortingOrder = i;
+
+                int _random = UnityEngine.Random.Range(0, 100);
+                if (_random <= 10)
                 {
-                    sr.sortingOrder = i;
+                    sr.sprite = tileSprites[1];
                 }
                 i--;
             }
 
-            // 한 줄 완성 후 딜레이
+            // 한줄 딜레이
             yield return new WaitForSeconds(0.1f);
         }
+
+        for (int y = 0; y >= -3; y--)
+        {
+            for (int x = width - 1; x >= 0; x--)
+            {
+                if (x >= 4 && x< 6)
+                {
+                    float xPos = (x - y) * tileWidth / 2.06f;
+                    float yPos = (x + y) * tileHeight / 2.06f;
+
+                    Vector3 spawnPos = startPos + new Vector3(xPos, yPos, 0f);
+
+                    GameObject tile = Instantiate(tilePrefab, spawnPos, Quaternion.identity, transform);
+
+                    SpriteRenderer sr = tile.GetComponentInChildren<SpriteRenderer>();
+                    sr.sortingOrder = i;
+                    i--;
+                }
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+
     }
 }
 
